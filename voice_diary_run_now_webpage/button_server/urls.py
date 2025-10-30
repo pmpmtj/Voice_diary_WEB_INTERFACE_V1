d50d5a9fe8422668a3a6951c9313b541cc785a6c
pmpmt <pmpmtj@hotmail.com>
2025-10-30 07:29:35 +0000

    commit

--- voice_diary_run_now_webpage/button_server/urls.py
+++
@@ -1,9 +0,0 @@
-"""
-URL configuration for button_server project.
-"""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('app.urls')),
-]
